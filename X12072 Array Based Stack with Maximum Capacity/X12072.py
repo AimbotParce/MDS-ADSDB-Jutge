0591f3e3c602d6cf29b8686dd840a4c1cbaa7d03
AimbotParce <marcparcerisa@gmail.com>
2024-10-12 19:29:56 +0200

    Fixed Size Array Based Stack

---
+++ X12072 Array Based Stack with Maximum Capacity/X12072.py
@@ -0,0 +1,196 @@
+import sys
+
+from yogi import scan
+
+
+class Empty(Exception):
+    """Error attempting to access an element from an empty container"""
+
+    pass
+
+
+class Full(Exception):
+    """Error attempting to add an element to a full container"""
+
+    pass
+
+
+class BaseArrayStack:
+    """LIFO Stack implementation using a Python list as underlying storage."""
+
+    def __init__(self):
+        """
+        Create an empty stack.
+
+        If maxlen <= 0, stack will not be able to exceed that length.
+        """
+        self._data = []
+
+    def __len__(self):
+        """Return the number of elements in the stack."""
+        return len(self._data)
+
+    def is_empty(self):
+        """Return True if the stack is empty."""
+        return len(self._data) == 0
+
+    @staticmethod
+    def _resize_check(previous, current):
+        if previous != current:
+            print(f"resized from {previous} to {current}")
+
+    def push(self, e):
+        """Add element e to the top of the stack."""
+        previous = sys.getsizeof(self._data)
+        self._data.append(e)
+        current = sys.getsizeof(self._data)
+        BaseArrayStack._resize_check(previous, current)
+
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack.
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty("Stack is empty")
+        return self._data[-1]
+
+    def pop(self):
+        """Remove and return the element from the top of the stack (i.e., LIFO).
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty("Stack is empty")
+        else:
+            previous = sys.getsizeof(self._data)
+            val = self._data.pop()
+            current = sys.getsizeof(self._data)
+            BaseArrayStack._resize_check(previous, current)
+        return val
+
+
+class FixedSizeArrayStack(BaseArrayStack):
+
+    def __init__(self, maxlen: int):
+        """
+        Create an empty stack.
+
+        If maxlen <= 0, stack will not be able to exceed that length.
+        """
+        self._data = [None] * maxlen
+        self._maxlen = maxlen
+        self._index = 0
+
+    def __len__(self):
+        """Return the number of elements in the stack."""
+        return self._index
+
+    def is_empty(self):
+        """Return True if the stack is empty."""
+        return len(self) == 0
+
+    def push(self, e):
+        """Add element e to the top of the stack."""
+        if len(self) == self._maxlen:
+            raise Full("Maxlen was already reached")
+        previous = sys.getsizeof(self._data)
+        self._data[self._index] = e
+        self._index += 1
+        current = sys.getsizeof(self._data)
+        BaseArrayStack._resize_check(previous, current)
+
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack.
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty("Stack is empty")
+        return self._data[self._index - 1]
+
+    def pop(self):
+        """Remove and return the element from the top of the stack (i.e., LIFO).
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty("Stack is empty")
+        else:
+            previous = sys.getsizeof(self._data)
+            self._index -= 1
+            val = self._data[self._index]  # No need to remove the element, it will
+            # be overridden on the next push operation.
+            current = sys.getsizeof(self._data)
+            BaseArrayStack._resize_check(previous, current)
+        return val
+
+
+def ArrayStack(maxlen: int = 0):
+    """
+    Create a stack using python lists. If maxlen is >= 0, a fixed memory will be
+    allocated for the stack. Otherwise, a dynamic list will be used.
+    """
+    if maxlen >= 0:
+        return FixedSizeArrayStack(maxlen=maxlen)
+    else:
+        return BaseArrayStack()
+
+
+if __name__ == "__main__":
+    n = scan(int)
+    while n is not None:
+        # s = ArrayStack()  # COMMENT THIS LINE IN YOUR ANSWER, AND UNCOMMENT NEXT LINE
+        s = ArrayStack(n)
+        # DO NOT MODIFY ANY OTHER PART OF THE PART OF THE __main__ FUNCTION
+        print(f"len {len(s)}")
+        if s.is_empty():
+            print("stack empty")
+
+        # pushing elements
+        for i in range(n + 1):
+            try:
+                s.push(i)
+                print(f"{i} pushed")
+            except Full:
+                print("push error: stack full")
+
+        # using accessors
+        print(f"len {len(s)}")
+        try:
+            print(f"top {s.top()}")
+        except:
+            print("top error: stack empty")
+
+        # popping elements
+        for _ in range(n // 4):
+            try:
+                e = s.pop()
+                print(f"{e} popped")
+            except Empty:
+                print("pop error: stack empty")
+
+        # using accessors
+        print(f"len {len(s)}")
+        try:
+            print(f"top {s.top()}")
+        except:
+            print("top error: stack empty")
+
+        # popping elements
+        for _ in range(2 + 3 * (n // 4)):
+            try:
+                e = s.pop()
+                print(f"{e} popped")
+            except Empty:
+                print("pop error: stack empty")
+
+        print(f"len {len(s)}")
+        try:
+            print(f"top {s.top()}")
+        except Empty:
+            print("top error: stack empty")
+
+        print()
+
+        n = scan(int)

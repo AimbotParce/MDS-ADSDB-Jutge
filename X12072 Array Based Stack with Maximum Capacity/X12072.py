0d032e362fa8a7a490f68b4c2dd6c6b64cc3d0b7
AimbotParce <marcparcerisa@gmail.com>
2024-10-15 12:43:38 +0200

    Fix inheritance on Array Based Stack with Maximum Capacity: Implement Protocols with generic typing.

--- X12072 Array Based Stack with Maximum Capacity/X12072.py
+++ X12072 Array Based Stack with Maximum Capacity/X12072.py
@@ -1,6 +1,12 @@
 import sys
+from typing import Any, Callable, ParamSpec, Protocol, Self, TypeVar
 
 from yogi import scan
+
+T = TypeVar("T", bound=Any)
+
+P = ParamSpec("P")
+R = TypeVar("R")
 
 
 class Empty(Exception):
@@ -15,7 +21,35 @@ class Full(Exception):
     pass
 
 
-class BaseArrayStack:
+class Stack(Protocol[T]):
+    _data: list[T]
+
+    def __init__(self, maxlen: int = 0) -> None: ...
+    def __len__(self) -> int: ...
+    def is_empty(self) -> bool: ...
+
+    def push(self, e: T) -> None: ...
+    def top(self) -> T: ...
+    def pop(self) -> T: ...
+
+    @staticmethod
+    def _resize_check(previous, current):
+        if previous != current:
+            print(f"resized from {previous} to {current}")
+
+    @staticmethod
+    def check_resize(method: Callable[P, R]) -> Callable[P, R]:  # pyright: ignore
+        def wrapper(self: Self, *args: P.args, **kwargs: P.kwargs) -> R:
+            previous = sys.getsizeof(self._data)
+            res = method(self, *args, **kwargs)
+            current = sys.getsizeof(self._data)
+            Stack._resize_check(previous, current)
+            return res
+
+        return wrapper
+
+
+class DynamicArrayStack(Stack):
     """LIFO Stack implementation using a Python list as underlying storage."""
 
     def __init__(self):
@@ -34,17 +68,10 @@ class BaseArrayStack:
         """Return True if the stack is empty."""
         return len(self._data) == 0
 
-    @staticmethod
-    def _resize_check(previous, current):
-        if previous != current:
-            print(f"resized from {previous} to {current}")
-
+    @Stack.check_resize
     def push(self, e):
         """Add element e to the top of the stack."""
-        previous = sys.getsizeof(self._data)
         self._data.append(e)
-        current = sys.getsizeof(self._data)
-        BaseArrayStack._resize_check(previous, current)
 
     def top(self):
         """Return (but do not remove) the element at the top of the stack.
@@ -55,6 +82,7 @@ class BaseArrayStack:
             raise Empty("Stack is empty")
         return self._data[-1]
 
+    @Stack.check_resize
     def pop(self):
         """Remove and return the element from the top of the stack (i.e., LIFO).
 
@@ -63,14 +91,11 @@ class BaseArrayStack:
         if self.is_empty():
             raise Empty("Stack is empty")
         else:
-            previous = sys.getsizeof(self._data)
             val = self._data.pop()
-            current = sys.getsizeof(self._data)
-            BaseArrayStack._resize_check(previous, current)
         return val
 
 
-class FixedSizeArrayStack(BaseArrayStack):
+class FixedArrayStack(Stack):
 
     def __init__(self, maxlen: int):
         """
@@ -90,15 +115,13 @@ class FixedSizeArrayStack(BaseArrayStack):
         """Return True if the stack is empty."""
         return len(self) == 0
 
+    @Stack.check_resize
     def push(self, e):
         """Add element e to the top of the stack."""
         if len(self) == self._maxlen:
             raise Full("Maxlen was already reached")
-        previous = sys.getsizeof(self._data)
         self._data[self._index] = e
         self._index += 1
-        current = sys.getsizeof(self._data)
-        BaseArrayStack._resize_check(previous, current)
 
     def top(self):
         """Return (but do not remove) the element at the top of the stack.
@@ -109,6 +132,7 @@ class FixedSizeArrayStack(BaseArrayStack):
             raise Empty("Stack is empty")
         return self._data[self._index - 1]
 
+    @Stack.check_resize
     def pop(self):
         """Remove and return the element from the top of the stack (i.e., LIFO).
 
@@ -117,24 +141,21 @@ class FixedSizeArrayStack(BaseArrayStack):
         if self.is_empty():
             raise Empty("Stack is empty")
         else:
-            previous = sys.getsizeof(self._data)
             self._index -= 1
             val = self._data[self._index]  # No need to remove the element, it will
             # be overridden on the next push operation.
-            current = sys.getsizeof(self._data)
-            BaseArrayStack._resize_check(previous, current)
         return val
 
 
-def ArrayStack(maxlen: int = 0):
+def ArrayStack(maxlen: int = 0) -> Stack:
     """
     Create a stack using python lists. If maxlen is >= 0, a fixed memory will be
     allocated for the stack. Otherwise, a dynamic list will be used.
     """
     if maxlen >= 0:
-        return FixedSizeArrayStack(maxlen=maxlen)
+        return FixedArrayStack(maxlen=maxlen)
     else:
-        return BaseArrayStack()
+        return DynamicArrayStack()
 
 
 if __name__ == "__main__":

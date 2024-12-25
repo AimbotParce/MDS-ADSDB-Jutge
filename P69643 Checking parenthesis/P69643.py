cf025dac27bf174c5ecc69d3c0609bc31ed003bc
AimbotParce <marcparcerisa@gmail.com>
2024-12-25 21:15:16 +0100

    Redo checking parenthesis. I don't know what was wrong, but it's okay now.

--- P69643 Checking parenthesis/P69643.py
+++ P69643 Checking parenthesis/P69643.py
@@ -2,35 +2,28 @@ import sys
 from collections import deque
 from typing import Iterable, TextIO
 
-# The list of "opening" and "closing" characters. Each opening character must be
-# in the same position in the "openers" list as their closing character is in
-# the "closers" one.
-openers = "[("
-closers = "])"
+# The mapping of opening-to-closing characters. The pairs are in the form of
+# "opening": "closing".
+mapping = {"[": "]", "(": ")"}
 # This means, "[" can only be closed with "]", and "(" can only be closed by ")"
 
 
 def checkParenthesis(line: str) -> bool:
     opener_stack: deque[str] = deque()
     for char in line:
-        # Check if the character is an opening or closing character, and find
-        # which "id" is it. This will allow us to identify which character is
-        # needed to close the current scope (the latest opening character in the
-        # stack).
-        opener_pos = openers.find(char)
-        closer_pos = closers.find(char)
-        if opener_pos != -1:
-            # If we encountered an opener, add one level to the stack
-            opener_stack.append(opener_pos)
-        elif opener_pos == -1 and closer_pos == -1:
-            # If it is neither, then we've found an illegal character.
-            raise ValueError(f"Encountered illegal character: {char}")
-        elif len(opener_stack) == 0 or closer_pos != opener_stack.pop():
-            # If we encountered a closer, check if it's the one that we were
-            # expecting. Otherwise, break early, this is incorrect.
-            return False
-        # It can't be both at the same time, because we've constructed the
-        # openers and closers list such that they are mutually exclusive.
+        expected_closer = mapping.get(char)
+        # This is the closer that we're expecting to find further down the line
+        if expected_closer is not None:
+            # Store it in the stack, so we can check it later
+            opener_stack.append(expected_closer)
+        else:
+            # If it's not an opener, it can be either a closer or an illegal
+            # character. Pop the last expected closer from the stack, if it
+            # matches the current character, we're good. Otherwise, it's either
+            # an illegal character or a mismatched closer. If the stack is empty,
+            # it cannot be okay.
+            if len(opener_stack) == 0 or char != opener_stack.pop():
+                return False
 
     # If we've ended, we only need to check whether we haven't left any unclosed scopes.
     return len(opener_stack) == 0

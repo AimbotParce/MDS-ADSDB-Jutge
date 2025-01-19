99be128b9db510e35f1bdabfb763eea5f7d84490
AimbotParce <marcparcerisa@gmail.com>
2025-01-19 13:45:52 +0100

    Statistical measures finally doesn't give time limit exceeded

--- P59282 Statistical measures/P59282-delivered.py
+++ P59282 Statistical measures/P59282-delivered.py
@@ -2,23 +2,23 @@ import heapq
 
 from yogi import scan
 
-
-def printStats(min_heap: list[int]):
-    if not min_heap:
-        print("no elements")
-        return
-    min_ = min_heap[0]
-    max_ = max(min_heap)
-    mean_ = sum(heap) / len(heap)
-    print(f"minimum: {min_}, maximum: {max_}, average: {mean_:.4f}")
-
-
 if __name__ == "__main__":
     heap: list[int] = []
+    current_max: int = -float("inf")
+    current_sum: int = 0
     while (instruction := scan(str)) is not None:
         if instruction == "number":
-            heapq.heappush(heap, scan(int))
+            new_number = scan(int)
+            current_max = max(current_max, new_number)
+            current_sum += new_number
+            heapq.heappush(heap, new_number)
         elif instruction == "delete":
             if len(heap) > 0:
-                heapq.heappop(heap)
-        printStats(heap)
+                elem = heapq.heappop(heap)
+                current_sum -= elem
+                if len(heap) == 0:
+                    current_max = -float("inf")  # reset the current max
+        if not heap:
+            print("no elements")
+        else:
+            print(f"minimum: {heap[0]}, maximum: {current_max}, average: {current_sum/len(heap):.4f}")
